70ecaf3be19616739d82077a29ec7b3e40e4bedd
Timeless <804093032@qq.com>
2020-09-22 23:19:14 +0800

    添加碧蓝航线SD小人

--- src/controller/spine.js
+++ src/controller/spine.js
@@ -1,6 +1,7 @@
 const Base = require('./base');
 const path = require('path');
 const fs = require('fs-extra');
+const { think } = require('thinkjs');
 
 module.exports = class extends Base {
   constructor(...arg) {
@@ -11,5 +12,24 @@ module.exports = class extends Base {
   async indexAction() {
     const sdList = await fs.readdir(this.baseurl);
     return this.success(sdList);
+  }
+
+  async updateAction() {
+    const output = think.getdirFiles(path.join(think.ASSETS_PATH, 'tiny/output'));
+    const sdList = await fs.readdir(this.baseurl);
+    sdList.forEach(item => {
+      const findPicture = output.find(element => element.includes(item));
+      if (findPicture) {
+        const src = path.join(think.ASSETS_PATH, 'tiny/output/' + findPicture);
+        if (think.isExist(src)) {
+          const source = path.join(think.ASSETS_PATH, 'tiny/', findPicture);
+          fs.remove(source);
+          const dest = path.join(this.baseurl, item, findPicture);
+          fs.move(src, dest, err => {
+            if (err) return console.error(err);
+          });
+        }
+      }
+    });
   }
 };
